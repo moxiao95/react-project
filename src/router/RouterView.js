2658c6087506181ec4fa084003fd0719bceffe57
moxiao <18838958405@126.com>
2021-03-05 16:52:01 +0800

    resetting: 重置文档

--- src/router/RouterView.js
+++ src/router/RouterView.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-
 import { HashRouter as Router, Link, Route } from 'react-router-dom';
-
-import Hello from '../views/Hello/Hello';
 
 const Home = () => (
     <div>
@@ -10,33 +7,11 @@ const Home = () => (
 	</div>
 );
 
-const About = () => (
-    <div>
-		<h2>About</h2>
-	</div>
-);
-
-const Product = () => (
-    <div>
-		<h2>Product</h2>
-	</div>
-);
-
 class App extends Component {
     render() {
         return (
             <Router>
-                <div className="App">
-                    <Link to="/">Home</Link>
-                    <Link to="/About">About</Link>
-                    <Link to="/Product">Product</Link>
-                    <Link to="/Hello">Hello</Link>
-                    <hr />
-                    <Route path="/" exact component={ Home }></Route>
-                    <Route path="/about" component={ About }></Route>
-                    <Route path="/product" component={ Product }></Route>
-                    <Route path="/hello" component={ Hello }></Route>
-                </div>
+                <Route path="/" component={ Home } />
             </Router>
         );
     }
